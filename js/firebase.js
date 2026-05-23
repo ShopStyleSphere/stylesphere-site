0158fdd56b6d965fee3b3b27c7ef9093f54ae12d
ShopStyleSphere <info@stylesphereofficial.com>
2026-05-23 23:16:15 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,35 +1,38 @@
 // Firebase Setup
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { 
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+import {
   getAuth,
   signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
+  createUserWithEmailAndPassword,
   sendEmailVerification
-} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
-import { 
+import {
   getFirestore,
   doc,
   setDoc,
   getDoc
-} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyCb4wbWGWSqaMFE5zYItWmUYBpXYKDGicw",
-    authDomain: "stylesphere-81dfe.firebaseapp.com",
-    projectId: "stylesphere-81dfe",
-    storageBucket: "stylesphere-81dfe.firebasestorage.app",
-    messagingSenderId: "545255416631",
-    appId: "1:545255416631:web:d4b82393bb70e92e05b0b2"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCb4vblwGWSqaMFE5zYITtWmUYBpXYKDGicw",
+  authDomain: "stylesphere-81dfe.firebaseapp.com",
+  projectId: "stylesphere-81dfe",
+  storageBucket: "stylesphere-81dfe.firebasestorage.app",
+  messagingSenderId: "545255416631",
+  appId: "1:545255416631:web:d4b82393bb70e92e05b0b2"
+};
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 export {
-  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
   sendEmailVerification,
   doc,
   setDoc,
